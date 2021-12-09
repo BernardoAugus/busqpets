dc183d091d87cb57c4d1e192892eb32b79f9a9db
Bruno Bavaresco <bebavaresco@gmail.com>
2021-12-08 23:34:29 -0300

    filtro

--- template/server/methods/produtos_methods.js
+++ template/server/methods/produtos_methods.js
@@ -9,21 +9,55 @@ Meteor.methods({
     Produtos.insert(produto);
   },
 
-  buscarProdutos() {
+  buscarProdutos(filter) {
     const user = Meteor.users.findOne({ _id: this.userId }, { fields: { 'profile.tipo': 1 } });
     const tipo = user?.profile?.tipo
     console.log('buscarProdutos')
     //sem parâmetros
     let produtos = { produtos: [], fornecedores: [] };
-    
+
+    const filterFornecedor = {};
+    const filterProduto = {};
+    let fornecedoresId = [];
+
+    if (filter.cidade) {
+      filterFornecedor.cidade = filter.cidade
+    }
+
+    if (filter.uf) {
+      filterFornecedor.uf = filter.uf
+    }
+
+    if (filter.bairro) {
+      filterFornecedor.bairro = { $regex: filter.bairro, $options: '-i' }
+    }
+
+    if (filter.nome) {
+      filterProduto.nome = { $regex: filter.nome, $options: '-i' }
+    }
+
+    if (filter.especies?.length > 0) {
+      filterProduto.especies = { $in: filter.especies }
+    }
+
     if (tipo == 1) {
       //é fornecedor
+      produtos.fornecedores = Meteor.users.find({ _id: this.userId }, { fields: { 'profile.name': 1, 'profile.endereco': 1 } }).fetch();
       produtos.produtos = Produtos.find({ fornecedor: this.userId }).fetch();
-      produtos.fornecedores = Meteor.users.find({ _id: this.userId }, { fields: { 'profile.name': 1 } }).fetch();
     } else {
       //é consumidor
-      produtos.produtos = Produtos.find({}).fetch();
-      produtos.fornecedores = Meteor.users.find({ tipo: 1 }, { fields: { 'profile.name': 1 } }).fetch();
+      filterFornecedor.tipo = 1;
+      produtos.fornecedores = Meteor.users.find(filterFornecedor, { fields: { 'profile.name': 1, 'profile.endereco': 1 } }).fetch();
+
+      if (Object.keys(filterFornecedor) > 1) {
+        for (fornecedor of produtos.fornecedores) {
+          fornecedoresId.push(fornecedor._id);
+        }
+
+        filterProduto.fornecedor = { $in: fornecedoresId }
+      }
+
+      produtos.produtos = Produtos.find(filterProduto).fetch();
     }
     console.log(produtos)
     return produtos;
